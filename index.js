51a9e31ae2e9987d3fa65e17e91aa6e1b9585bf1
vyron <fiee_vyron@163.com>
2020-12-12 15:31:39 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: false,
+	semi: true,
+	singleQuote: false,
+	jsxSingleQuote: false,
+	quoteProps: "as-needed",
+	trailingComma: "none",
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	arrowParens: "avoid",
+	requirePragma: false,
+	insertPragma: false,
+	proseWrap: "never",
+	htmlWhitespaceSensitivity: "css",
+	vueIndentScriptAndStyle: false,
+	endOfLine: "lf",
+	embeddedLanguageFormatting: "auto"
+}
